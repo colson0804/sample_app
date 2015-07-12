c7cbb335b5a0f3f447a637a7c10de1f61a449918
colson0804 <craigolson2015@u.northwestern.edu>
2015-07-12 14:34:55 -0500

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 		uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }, presence: true, allow_nil: true
 	has_secure_password
+  has_many :microposts, dependent: :destroy
 
 	# Returns the hash digest of the given string.
   def User.digest(string)
@@ -63,6 +64,10 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id=?", id)
   end
 
   private 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,6 +22,15 @@ class ActiveSupport::TestCase
   	end
   end
 
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+
   private
   	def integration_test?
   		defined?(post_via_redirect)
